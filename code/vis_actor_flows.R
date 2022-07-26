a6c530f48f54962efbe5130cdcf346558557702e
Thomas Brailey <thomasjbrailey@gmail.com>
2022-07-26 11:54:44 +0100

    Create vis_actor_flows.R

---
+++ code/vis_actor_flows.R
@@ -0,0 +1,29 @@
+######################################################################################################################################################
+######
+
+plot <- ggplot() + 
+  geom_sf(data = df_world_map, fill = NA) + 
+  geom_curve(
+    data = df_acled_final, 
+    color = "purple",
+    alpha = 0.5, 
+    curvature = 0.2,
+    aes(
+      x = orig_lon,
+      y = orig_lat,
+      xend = longitude,
+      yend = latitude)) + 
+  coord_sf(xlim = c(-40, 60), ylim = c(-30, 70)) + 
+  geom_point(
+    data = df_acled_final, 
+    mapping = aes(x = longitude, y = latitude), 
+    color = "purple", 
+    alpha = 0.5, show.legend = TRUE) +
+  geom_point(
+    data = df_acled_final, 
+    mapping = aes(x = orig_lon,  y = orig_lat),
+    color = "orange", 
+    alpha = 0.5) + 
+  labs(x = "", y = "", color = "Test") + 
+  theme_classic()
+plot
