e332b6cc454d29756f5cf13d50971eef6d258526
Thomas Brailey <thomasjbrailey@gmail.com>
2022-07-26 09:19:28 +0100

    Update main.R

--- code/main.R
+++ code/main.R
@@ -8,7 +8,9 @@ gc()
 library(ggplot2)
 library(magrittr)
 
-googlesheets4::gs4_get(email = "thomasjbrailey@gmail.com")
+googlesheets4::gs4_deauth()
+
+googlesheets4::gs4_auth(email = "thomasjbrailey@gmail.com")
 
 # file paths
 fp_main <- here::here()
@@ -18,10 +20,10 @@ fp_code <- paste0(fp_main, "/code")
 # read data 
 df_acled        <- read.csv(paste0(fp_data, "/acled_conflict_africa_1997_2022.csv"))
 
-vec_ss    <- "https://docs.google.com/spreadsheets/d/1SQsSwuNcle_i1laXZmtxNutgi5VH27WF/edit#gid=96111818"
-vec_years <- c(1997:2022)
+vec_ss    <- "https://docs.google.com/spreadsheets/d/1Mr0AcsLRywOo5BmyE7ahfKP9XZVKYW7j6VQvltyo37Q/edit#gid=96111818"
+vec_years <- c(as.character(1997:2022))
 
-df_acled_manual <- sapply(X = seq_along(vec_years), FUN = function(x) googlesheets4::read_sheet(ss = vec_ss, sheet = vec_years[x]))
+df_acled_manual <- googlesheets4::read_sheet(ss = vec_ss, sheet = vec_years[1])
 
 ######################################################################################################################################################
 ###### read scripts
