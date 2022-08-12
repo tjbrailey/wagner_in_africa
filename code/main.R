5017e770c03db89e48c49302bc258debcc1894b2
Thomas Brailey <thomasjbrailey@gmail.com>
2022-08-12 09:22:39 +0100

    Update main.R

--- code/main.R
+++ code/main.R
@@ -8,6 +8,8 @@ gc()
 library(ggplot2)
 library(magrittr)
 
+windowsFonts(font = windowsFont("Times New Roman"))
+
 googlesheets4::gs4_deauth()
 
 googlesheets4::gs4_auth(email = "thomasjbrailey@gmail.com")
@@ -16,16 +18,13 @@ googlesheets4::gs4_auth(email = "thomasjbrailey@gmail.com")
 fp_main <- here::here()
 fp_data <- paste0(fp_main, "/data")
 fp_code <- paste0(fp_main, "/code")
+fp_exhibits <- paste0(fp_main, "/exhibits")
+fp_tables   <- paste0(fp_exhibits, "/tables")
+fp_figures  <- paste0(fp_exhibits, "/figures")
 
 # read data 
 df_acled     <- read.csv(paste0(fp_data, "/acled_conflict_africa_1997_2022.csv")) # raw acled data
 df_world_map <- rnaturalearth::ne_countries(scale = "medium", type = "map_units", returnclass = "sf")
-
-# read googlesheet-stored datasets
-vec_ss <- "https://docs.google.com/spreadsheets/d/1m1f2efMV61YMtlz1ugsQnPMCB0XLfIlMV1m_N8KGCbk/edit#gid=179470013" 
-vec_years <- c(as.character(1997:2022))
-
-df_acled_manual <- googlesheets4::read_sheet(ss = vec_ss, sheet = vec_years[1])
 
 ######################################################################################################################################################
 ###### wrangle data 
